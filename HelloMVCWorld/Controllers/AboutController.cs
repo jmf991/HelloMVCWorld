abfd3de282188beeb329cc2af474f1e27fabc6a4
jmf0991 <jmf0991@gmail.com>
2019-07-16 11:27:40 +0200

    added controllers and models for about and products

---
+++ HelloMVCWorld/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVCWorld.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> _casesStudies = new List<string>();
+            _casesStudies.Add("Case Studiy 1");
+            Models.About _about = new Models.About()
+            {
+                Intro = "This is the intro for the about page",
+                CaseStudies = _casesStudies
+            };
+            return View(_about);
+        }
+    }
+}
---
+++ HelloMVCWorld/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVCWorld.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            Models.Product _product = new Models.Product()
+            {
+                Name = "Product 1",
+                Price = 6
+            };
+            return View(_product);
+        }
+    }
+}
