412e2188d8cf0ed58cd46fa23f2b58662ccb5068
jmf0991 <jmf0991@gmail.com>
2019-07-16 11:40:12 +0200

    added routing

--- HelloMVCWorld/Startup.cs
+++ HelloMVCWorld/Startup.cs
@@ -26,6 +26,11 @@ namespace HelloMVCWorld
                 app.UseDeveloperExceptionPage();
             }
             app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("Products", "Products/{action=Index}/{id?}", new { controller = "Products" });
+                routes.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
