b48cf6d6b0d28ab5171a0656eb3a81ea1f7cf2bb
jmf0991 <jmf0991@gmail.com>
2019-07-17 11:27:34 +0200

    added cookies controller

---
+++ HelloMVCWorld/Controllers/CookiesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVCWorld.Controllers
+{
+    public class CookiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (!HttpContext.Request.Cookies.ContainsKey("first_request"))
+            {
+                HttpContext.Response.Cookies.Append("first_request", DateTime.Now.ToString());
+                return Content("Welcome, new visitor!");
+            }
+            else
+            {
+                DateTime firstRequest = DateTime.Parse(HttpContext.Request.Cookies["first_request"]);
+                return Content("Welcome back, user! You first visited us on: " + firstRequest.ToString());
+            }
+        }
+    }
+}
