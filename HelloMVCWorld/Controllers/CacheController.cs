8f7d98b6f172fe2762d7f0fdb842245d1bb410f6
jmf0991 <jmf0991@gmail.com>
2019-07-17 11:36:05 +0200

    cacheController added

---
+++ HelloMVCWorld/Controllers/CacheController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVCWorld.Controllers
+{
+    public class CacheController : Controller
+    {
+        //browser requesting the page will be given permission to cache the response of the request for 2 minutes (120 seconds)
+        [ResponseCache(Duration = 120)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
